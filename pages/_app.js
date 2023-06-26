07a448d3362d6da9c000e360b273a87afb73fd0c
김선미 <zlatjs2@gmail.com>
2023-06-26 14:38:11 +0900

    test : 리코일 등 세팅 테스트

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { RecoilRoot } from "recoil";
+// import Auth from "@/components/init/Auth";
+// import Analytics from "@/components/init/Analytics";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
+
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        {/* <Auth> */}
+        <Component {...pageProps} />
+        <div id="modal-root"></div>
+        {/* </Auth> */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
