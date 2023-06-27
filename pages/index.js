6942e66dc2169107bef2b5df456a5fa25150698b
김선미 <zlatjs2@gmail.com>
2023-06-27 15:54:17 +0900

    test 5

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       {[1, 2, 3, 4, 5].map((item, idx) => (
         <Link href={`/about/?test=${item}`} key={idx}>
           링크이동 - {item}
         </Link>
+      ))}
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      {[1, 2, 3, 4, 5].map((item, idx) => (
+        <button
+          type="button"
+          onClick={() =>
+            router.push({ pathname: "/about", query: { test: item } })
+          }
+          key={idx}
+        >
+          버튼링크 이동{item}
+        </button>
       ))}
     </>
   );
