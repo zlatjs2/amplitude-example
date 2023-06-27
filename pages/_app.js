a54a4903d9c55d255d11323098c9a766be4876e8
김선미 <zlatjs2@gmail.com>
2023-06-27 15:00:09 +0900

    test 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 
 import { RecoilRoot } from "recoil";
-import AuthComponent from "@/components/AuthComponent";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -18,10 +17,8 @@ export default function App({ Component, pageProps }) {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <AuthComponent>
-          <Component {...pageProps} />
-          <div id="modal-root"></div>
-        </AuthComponent>
+        <Component {...pageProps} />
+        <div id="modal-root"></div>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
