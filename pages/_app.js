d654df485c1fe0eadbdc97037d69e8a65eae4db3
김선미 <zlatjs2@gmail.com>
2023-06-27 15:44:27 +0900

    test 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,10 @@
 import "@/styles/globals.css";
 
-import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: 0, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
-    },
-  },
-});
-
 export default function App({ Component, pageProps }) {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <div id="modal-root"></div>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <>
+      <Component {...pageProps} />
+      <div id="modal-root"></div>
+    </>
   );
 }
